861e91ab3b5dfe9f0d25fbaa1b6999fa1693de8f
Jens Geudens <jensgeudens@hotmail.com>
2015-03-06 20:05:57 +0100

    Add extra colors (thanks to @pluyckx)

--- src/scopegui.cpp
+++ src/scopegui.cpp
@@ -8,13 +8,24 @@
 #include "qcustomplot.h"
 
 
-const QList<QColor> ScopeGui::_colorlist = QList<QColor>() << QColor("blue")
-                                                           << QColor("green")
-                                                           << QColor("red")
-                                                           << QColor("yellow")
-                                                           << QColor("orange")
-                                                           << QColor("purple")
-                                                           << QColor("black")
+const QList<QColor> ScopeGui::_colorlist = QList<QColor>() << QColor(0, 0, 0)
+                                                           << QColor(0, 0, 255)
+                                                           << QColor(0, 255, 255)
+                                                           << QColor(0, 255, 0)
+                                                           << QColor(220, 220, 0)
+                                                           << QColor(220, 153, 14)
+                                                           << QColor(255, 165, 0)
+                                                           << QColor(255, 0, 0)
+                                                           << QColor(255, 160, 122)
+                                                           << QColor(230, 104, 86)
+                                                           << QColor(205, 205, 180)
+                                                           << QColor(157, 153, 120)
+                                                           << QColor(139, 69, 19)
+                                                           << QColor(255, 20, 147)
+                                                           << QColor(74, 112, 139)
+                                                           << QColor(46, 139, 87)
+                                                           << QColor(128, 0, 128)
+                                                           << QColor(189, 183, 107)
                                                            ;
 
 ScopeGui::ScopeGui(MainWindow *window, ScopeData *scopedata, QCustomPlot * pPlot, QObject *parent) :
