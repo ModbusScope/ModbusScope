d273f0760a97d882a9df0927f6eb5149b7823439
Jens Geudens <jensgeudens@hotmail.com>
2018-07-21 22:42:15 +0200

    Make sure debug information is shown
    Disabled by default in Ubuntu

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,9 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     QCoreApplication::setApplicationName("ModbusScope");
 
+    /* Make sure debug messages are printed in console (disabled by default in Ubuntu) */
+    QLoggingCategory::defaultCategory()->setEnabled(QtDebugMsg, true);
+
     MainWindow w(a.arguments());
 
     w.show();
