e8b71ddaf6b694bcb18c5933e0a366701831b7bd
Jens Geudens <jensgeudens@hotmail.com>
2019-01-02 22:12:37 +0100

    Add and rework Connection tests

--- tests_integration/communication/modbusconnection/testmodbusconnection.h
+++ tests_integration/communication/modbusconnection/testmodbusconnection.h
@@ -1,12 +1,33 @@
 
 #include <QObject>
+#include <QPointer>
+#include <QUrl>
+#include "settingsmodel.h"
+
+#include "testslavedata.h"
+#include "testslavemodbus.h"
+
 
 class TestModbusConnection: public QObject
 {
     Q_OBJECT
 private slots:
-    void initTestCase();
+    void init();
+    void cleanup();
+
     void connectionSuccess();
     void connectionFail();
     void connectionSuccesAfterFail();
+
+    void readRequestSuccess();
+    void readRequestProtocolError();
+    void readRequestError();
+
+private:
+
+    QPointer<TestSlaveData> _pTestSlaveData;
+    QPointer<TestSlaveModbus> _pTestSlaveModbus;
+
+    SettingsModel _settingsModel;
+    QUrl _serverConnectionData;
 };
