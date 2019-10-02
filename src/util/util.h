2daede3447b5d84e888a8bd450d2b02665499a11
Jens Geudens <j.geudens@edna.eu>
2019-10-02 13:15:13 +0200

    Fix bug when separator character is the same as decimal point in locale

--- src/util/util.h
+++ src/util/util.h
@@ -5,6 +5,7 @@
 #include <QMessageBox>
 #include <QLocale>
 #include <QColor>
+#include <QDebug>
 #include <QDateTime>
 
 class Util : public QObject
@@ -44,15 +45,29 @@ public:
     static QString formatDoubleForExport(double number)
     {
         QString tmp;
+        
+        /* TODO: Can't this be done without converting to string and back and back again? */
 
-        // Round number to 3 decimals and remove group separator
-        tmp = QLocale::system().toString(number, 'f', 3).replace(QLocale::system().groupSeparator(), "");
+        // Round number to 3 decimals
+        tmp = QLocale::system().toString(number, 'f', 3);
+
+        // Remove group separator
+        if (QLocale::system().groupSeparator() != QLocale::system().decimalPoint())
+        {
+            tmp = tmp.replace(QLocale::system().groupSeparator(), "");
+        }
 
         // convert back to double
         double doubleTmp = QLocale::system().toDouble(tmp);
 
-        // Make sure trailing zeros are removed and remove group separator
-        tmp = QLocale::system().toString(doubleTmp, 'g', 9).replace(QLocale::system().groupSeparator(), "");
+        // Make sure trailing zeros are removed
+        tmp = QLocale::system().toString(doubleTmp, 'g', 9);
+
+        // Remove group separator
+        if (QLocale::system().groupSeparator() != QLocale::system().decimalPoint())
+        {
+            tmp = tmp.replace(QLocale::system().groupSeparator(), "");
+        }
 
         return tmp;
     }
