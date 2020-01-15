630fbd4b43c5d956cb7ac6ce08c56a14ce9d466b
Jens Geudens <jensgeudens@hotmail.com>
2020-01-15 22:22:34 +0100

    Update tests (mbc import)

--- tests_integration/importexport/mbctestdata.cpp
+++ tests_integration/importexport/mbctestdata.cpp
@@ -16,8 +16,8 @@ QStringList MbcTestData::cSingleTab_TabList =
                     QStringList() << "Tab1";
 
 QList <MbcRegisterData> MbcTestData::cSingleTab_RegList =
-                    QList <MbcRegisterData>() << MbcRegisterData(40001, true, "Reg1", 0, false, true)
-                                              << MbcRegisterData(40002, true, "Reg2", 0, false, true);
+                    QList <MbcRegisterData>() << MbcRegisterData(40001, true, "Reg1", 0, false, true, 0)
+                                              << MbcRegisterData(40002, true, "Reg2", 0, false, true, 0);
 
 
 QString MbcTestData::cMultiTab = QString(
@@ -44,9 +44,9 @@ QStringList MbcTestData::cMultiTab_TabList =
                                   << "Tab3";
 
 QList <MbcRegisterData> MbcTestData::cMultiTab_RegList =
-                    QList <MbcRegisterData>() << MbcRegisterData(40001, true, "Reg1", 0, false, true)
-                                              << MbcRegisterData(40002, true, "Reg2", 1, false, true)
-                                              << MbcRegisterData(40003, true, "Reg3", 2, false, true);
+                    QList <MbcRegisterData>() << MbcRegisterData(40001, true, "Reg1", 0, false, true, 0)
+                                              << MbcRegisterData(40002, true, "Reg2", 1, false, true, 0)
+                                              << MbcRegisterData(40003, true, "Reg3", 2, false, true, 0);
 
 QString MbcTestData::cRegisterOptions = QString(
     "﻿<?xml version=\"1.0\" encoding=\"utf-8\"?>"                                        "\n"\
@@ -58,6 +58,8 @@ QString MbcTestData::cRegisterOptions = QString(
     "	<var><reg>40003</reg><text>Reg3</text><type>uint16</type><rw>w</rw></var>"      "\n"\
     "	<var><reg>40004</reg><text>Reg4</text><type>uint32</type><rw>r</rw></var>"      "\n"\
     "	<var><reg>40006</reg><text>Reg5</text><type>uint16</type><rw>r</rw></var>"      "\n"\
+    "	<var><reg>40007</reg><text>Reg6</text><type>uint16</type><decimals>0</decimals><rw>r</rw></var>"      "\n"\
+    "	<var><reg>40008</reg><text>Reg7</text><type>uint16</type><decimals>5</decimals><rw>r</rw></var>"      "\n"\
     "</tab>"                                                                            "\n"\
     "</modbuscontrol>"                                                                  "\n"\
 );
@@ -65,9 +67,13 @@ QString MbcTestData::cRegisterOptions = QString(
 QStringList MbcTestData::cRegisterOptions_TabList = QStringList() << "Tab1";
 
 QList <MbcRegisterData> MbcTestData::cRegisterOptions_RegList =
-                    QList <MbcRegisterData>() << MbcRegisterData(40001, true, "Reg1", 0, false, true)
-                                              << MbcRegisterData(40002, false, "Reg2", 0, false, true)
-                                              << MbcRegisterData(40003, true, "Reg3", 0, false, false)
-                                              << MbcRegisterData(40004, true, "Reg4", 0, true, true)
-                                              << MbcRegisterData(40006, true, "Reg5", 0, false, true);
+                    QList <MbcRegisterData>() << MbcRegisterData(40001, true, "Reg1", 0, false, true, 0)
+                                              << MbcRegisterData(40002, false, "Reg2", 0, false, true, 0)
+                                              << MbcRegisterData(40003, true, "Reg3", 0, false, false, 0)
+                                              << MbcRegisterData(40004, true, "Reg4", 0, true, true, 0)
+                                              << MbcRegisterData(40006, true, "Reg5", 0, false, true, 0)
+
+                                              << MbcRegisterData(40007, true, "Reg6", 0, false, true, 0)
+                                              << MbcRegisterData(40008, true, "Reg7", 0, false, true, 5)
+                                                ;
 
