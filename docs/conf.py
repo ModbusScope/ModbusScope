11d7cb1aaa00d32e1e62efe9f752dace34d474bf
Jens Geudens <jensgeudens@hotmail.com>
2020-08-22 14:02:09 +0200

    Add logo

--- docs/conf.py
+++ docs/conf.py
@@ -55,3 +55,10 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_logo = "../resources/icon/icon_original.svg"
+html_favicon = "../resources/icon/application.ico"
+html_theme_options = {
+    'logo_only': False,
+    'display_version': True,
+}
