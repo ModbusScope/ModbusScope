a9a07738ece66ab158d74fdcb1bc00c65db17692
Jens Geudens <jensgeudens@hotmail.com>
2020-11-10 17:01:14 +0100

    Add script to generate docs locally

---
+++ create_doc.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+VENV_DIR="venv"
+DOCS_FOLDER="docs"
+
+cd $DOCS_FOLDER
+
+if [ -d "$VENV_DIR" ]; then
+  # Take action if $DIR exists. #
+
+  python3 create_venv.py --requirements requirements.txt
+
+fi
+
+source venv/bin/activate
+
+make html
