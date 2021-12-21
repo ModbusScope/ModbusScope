f3b4e9b8b2091ea56d067d6108bc5e6f857cc0a1
Jens Geudens <jensgeudens@hotmail.com>
2021-12-21 20:48:58 +0100

    Update open mbs functions
    Remain backwards compatible with older project files

--- src/importexport/projectfileparser.cpp
+++ src/importexport/projectfileparser.cpp
@@ -52,9 +52,11 @@ bool ProjectFileParser::parseFile(QString& fileContent, ProjectSettings *pSettin
 
             if (bRet)
             {
-                if (datalevel != ProjectFileDefinitions::cCurrentDataLevel)
+                pSettings->dataLevel = datalevel;
+                if (datalevel > ProjectFileDefinitions::cCurrentDataLevel)
                 {
-                    Util::showError(tr("Data level (%1) is not supported. Only datalevel 2 is allowed. Project file loading is aborted.").arg(datalevel));
+                    Util::showError(tr("Data level (%1) is not supported. Only datalevel %2 or lower is supported.\nProject file loading is aborted.")
+                                       .arg(datalevel,ProjectFileDefinitions::cCurrentDataLevel));
                     bRet = false;
                 }
             }
