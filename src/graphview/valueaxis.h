5e5e8b66852471446dc3dc72753820bcbfd825d4
Jens Geudens <jensgeudens@hotmail.com>
2022-03-11 20:00:53 +0100

    Remove not working code

--- src/graphview/valueaxis.h
+++ src/graphview/valueaxis.h
@@ -10,14 +10,6 @@ public:
     ValueAxis(QCPAxisRect *parent, AxisType type);
     void rescaleValue(QCPRange keyAxisRange);
 
-    void setRange(double lower, double upper);
-
-public slots:
-    void setRange(const QCPRange &range);
-
-private:
-    void addMargin(double * pLower, double * pUpper);
-
 };
 
 #endif // VALUEAXIS_H
