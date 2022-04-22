e281fd3b687d079a2a0769a3aec89398cab11350
Jens Geudens <jensgeudens@hotmail.com>
2022-04-22 21:56:15 +0200

    Add caching for Windows Qt installation

--- scripts/full_build_and_deploy_windows.bat
+++ scripts/full_build_and_deploy_windows.bat
@@ -1,5 +1,9 @@
 echo on
 
-call scripts\setup_windows.bat %1
+REM Usage (path is optional):
+REM Use cache: scripts\full_build_and_deploy_windows.bat 'true' [path]
+REM No cache: scripts\full_build_and_deploy_windows.bat '' [path]
+
+call scripts\setup_windows.bat %1 %2
 call scripts\build_windows.bat
 call scripts\deploy_windows.bat
