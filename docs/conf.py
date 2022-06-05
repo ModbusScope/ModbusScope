2767281c83f5622261a1560aabf7dc8861169df4
Jens Geudens <jensgeudens@hotmail.com>
2022-06-05 20:06:17 +0200

    Update version

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ import sphinx_rtd_theme
 # -- Project information -----------------------------------------------------
 
 project = 'ModbusScope'
-copyright = '2021, Jens Geudens'
+copyright = '2022, Jens Geudens'
 author = 'Jens Geudens'
 
 
