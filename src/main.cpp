a1276f5cefb1e2e62eb53ecc06006bffe65f785c
Jens Geudens <jensgeudens@hotmail.com>
2022-09-30 19:12:38 +0200

    Add global mutex to prevent installer to run when already active

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,18 @@
 #include <QApplication>
-
 #include "mainapp.h"
+
+#ifdef WIN32
+#include "Synchapi.h"
+#endif
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     QCoreApplication::setApplicationName("ModbusScope");
+
+#ifdef WIN32
+    CreateMutexA(NULL, FALSE, "ModbusScope");
+#endif
 
     MainApp app(a.arguments());
 
