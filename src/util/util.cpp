35928aa97f2a36b9b264209ca9154f356b130cc1
Jens Geudens <jensgeudens@hotmail.com>
2025-12-04 20:28:29 +0100

    Update colours

--- src/util/util.cpp
+++ src/util/util.cpp
@@ -1,22 +1,32 @@
 
 #include "util.h"
 
+/* For Markdown rendering
+<span style="display:inline-block; width:20px; height:20px; background: rgb(0, 0, 0);"></span>
+<span style="display:inline-block; width:20px; height:20px; background: rgb(0, 0, 255);"></span>
+<span style="display:inline-block; width:20px; height:20px; background: rgb(0, 255, 255);"></span>
+<span style="display:inline-block; width:20px; height:20px; background: rgb(0, 255, 0);"></span>
+<span style="display:inline-block; width:20px; height:20px; background: rgba(255, 40, 40, 1);"></span>
+<span style="display:inline-block; width:20px; height:20px; background: rgb(128, 0, 128);"></span>
+<span style="display:inline-block; width:20px; height:20px; background: rgba(112, 49, 3, 1);"></span>
+<span style="display:inline-block; width:20px; height:20px; background: rgba(250, 2, 134, 1);"></span>
+<span style="display:inline-block; width:20px; height:20px; background: rgba(45, 126, 184, 1);"></span>
+<span style="display:inline-block; width:20px; height:20px; background: rgba(0, 170, 74, 1);"></span>
+<span style="display:inline-block; width:20px; height:20px; background: rgb(220, 220, 0);"></span>
+<span style="display:inline-block; width:20px; height:20px; background: rgb(255, 165, 0);"></span>
+*/
+
+// clang-format off
 const QList<QColor> Util::cColorlist = QList<QColor>() << QColor(0, 0, 0)
-                                                           << QColor(0, 0, 255)
-                                                           << QColor(0, 255, 255)
-                                                           << QColor(0, 255, 0)
-                                                           << QColor(220, 220, 0)
-                                                           << QColor(220, 153, 14)
-                                                           << QColor(255, 165, 0)
-                                                           << QColor(255, 0, 0)
-                                                           << QColor(255, 160, 122)
-                                                           << QColor(230, 104, 86)
-                                                           << QColor(205, 205, 180)
-                                                           << QColor(157, 153, 120)
-                                                           << QColor(139, 69, 19)
-                                                           << QColor(255, 20, 147)
-                                                           << QColor(74, 112, 139)
-                                                           << QColor(46, 139, 87)
-                                                           << QColor(128, 0, 128)
-                                                           << QColor(189, 183, 107)
-                                                           ;
+                                                       << QColor(0, 0, 255)
+                                                       << QColor(0, 255, 255)
+                                                       << QColor(0, 255, 0)
+                                                       << QColor(255, 40, 40)
+                                                       << QColor(128, 0, 128)
+                                                       << QColor(112, 49, 3)
+                                                       << QColor(250, 2, 134)
+                                                       << QColor(45, 126, 184)
+                                                       << QColor(0, 170, 74)
+                                                       << QColor(220, 220, 0)
+                                                       << QColor(255, 165, 0);
+// clang-format on
