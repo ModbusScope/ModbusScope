0d1d0e8149c977c1a3b1d347be081980c92ffd9c
Jens Geudens <jensgeudens@hotmail.com>
2025-12-05 16:01:17 +0100

    Update generated files

--- docs/manual/conf.py
+++ docs/manual/conf.py
@@ -1,22 +1,11 @@
 # Configuration file for the Sphinx documentation builder.
 #
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
+# For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-import sphinx_rtd_theme
 
 
 # -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'ModbusScope'
 copyright = '2025, Jens Geudens'
@@ -24,10 +13,8 @@ author = 'Jens Geudens'
 
 
 # -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = ["sphinx_rtd_theme",
               "myst_parser",
               "sphinx.ext.imgconverter",
@@ -51,6 +38,7 @@ source_suffix = {
 master_doc = 'index'
 
 # -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
--- docs/manual/index.rst
+++ docs/manual/index.rst
@@ -1,10 +1,10 @@
 .. ModbusScope documentation master file, created by
-   sphinx-quickstart on Fri Aug 21 20:21:23 2020.
+   sphinx-quickstart on Fri Dec  5 14:46:11 2025.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to ModbusScope's documentation!
-=======================================
+ModbusScope documentation
+=========================
 
 .. include:: intro.md
    :parser: commonmark
