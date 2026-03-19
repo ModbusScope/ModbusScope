df9d97c04189f3bebe384108d47479dd715fc927
Jens Geudens <jensgeudens@hotmail.com>
2026-03-19 19:45:43 +0100

    Make sure all autogen headers are created

--- scripts/run_clazy.sh
+++ scripts/run_clazy.sh
@@ -10,6 +10,9 @@ cmake -GNinja \
     -DCMAKE_PREFIX_PATH="${QT_PREFIX}" \
     -B "${BUILD_DIR}"
 
+echo "=== Generating autogen headers ==="
+ninja -C "${BUILD_DIR}" src/ScopeSource_autogen
+
 echo "=== Running clazy ==="
 find "$(pwd)/src" -name "*.cpp" -print0 | \
     xargs -0 -P "$(nproc)" -I{} clazy-standalone --only-qt --header-filter="src/.*" -p "${BUILD_DIR}" {}
